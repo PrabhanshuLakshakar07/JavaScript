53d8e665958e0b4165cf929d7bade8d017291185
PrabhanshuLakshakar07 <lakshakarprabhanshu@gmail.com>
2026-03-26 15:15:21 +0530

    function

---
+++ JavaScript/source.js
@@ -0,0 +1,7 @@
+ function fun1(){
+    var result=confirm("press any key ");
+    if(result==true)
+    document.write("You Have Pressed OK button");
+    else
+    document.write("you have cancel button");
+  }
